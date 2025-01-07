a98d9982abd98ea06b47983d5810505ce0f376a2
AbdoulayeTheSpy <njigouhrazak@iut-dhaka.edu>
2025-01-07 18:24:02 +0100

    ignore rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable the rule for using the 'any' type
+      '@typescript-eslint/no-explicit-any': 'off',
+      
+      // Disable the rule for unused variables
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
